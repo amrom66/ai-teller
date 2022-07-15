862b608179a62242995a52a42273cdf119d2d0bc
amrom <linjinbao666@gmail.com>
2022-07-15 16:40:20 +0800

    updarte

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 from ast import arg
 import util.host
 import web.home
+import util.camera
 
 def cli():
     parser = argparse.ArgumentParser(description='ai-teller is a program to tell you your future')
@@ -15,6 +16,10 @@ def cli():
     server_command = subparsers.add_parser("server", help="启动服务器")
     server_command.add_argument('-p', '--port', type=int, default=9870, required=False, help='端口')
     server_command.set_defaults(handle=handle_server)
+    
+    # opencv command and its arguments
+    opencv_command = subparsers.add_parser("opencv", help="启动opencv")
+    opencv_command.set_defaults(handle=handle_opencv)
 
     migrate_command= subparsers.add_parser("migrate",help="迁移数据")
     migrate_command.add_argument('-src', '--source', type= str,default='ikk', required= True, help ='数据源')
@@ -39,5 +44,10 @@ def handle_server(args):
     print('server will listen on ip:', ip)
     print('server will listen on port:', args.port)
     web.home.app.run(host=ip, port=args.port)
+
+def handle_opencv(args):
+    print('opencv will open your camera')
+    util.camera.video()
+
 if __name__ == '__main__':
     cli()
